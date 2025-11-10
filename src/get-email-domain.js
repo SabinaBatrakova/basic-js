f293d3f60d64c179d84981bd3e672a659a25400f
Sabina Batrakova <mms8818@mail.ru>
2025-11-10 17:15:02 +0100

    feat: add get-email-domains solution

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given an email address, return it's domain.
@@ -10,10 +10,23 @@ const { NotImplementedError } = require('../lib');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
+function getEmailDomain(email) {
+  //throw new NotImplementedError('Not implemented');
+  /*
+  const chars = [...String(email)];
+
+  for (let i = 0; i < chars.length; i++) {
+    let copy = [...chars];
+    const findIndex = copy.lastIndexOf("@");
+    copy.slice(copy[0], findIndex);
+    return String(copy.join());
+  }*/
+
+  const charsOfEmail = String(email).trim();
+  const findIndex = charsOfEmail.lastIndexOf("@");
+  return charsOfEmail.slice(findIndex + 1);
 }
 
 module.exports = {
-  getEmailDomain
+  getEmailDomain,
 };

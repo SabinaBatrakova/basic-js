df2027065023c0cff088a8cb04d79fb5b416a001
Sabina Batrakova <mms8818@mail.ru>
2025-11-10 21:53:42 +0100

    feat: add simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,30 +1,47 @@
-const { decorateObject } = require('../lib');
-const { NotImplementedError } = require('../lib');
+const { decorateObject } = require("../lib");
+const { NotImplementedError } = require("../lib");
 
 /**
  * Implement chainMaker object according to task description
  *
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    //возвращает текущую длину цепочки в виде числа;
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    //добавляет ссылку, содержащую string представление valueв цепочку;
+    this.chain.push(`( ${value !== undefined ? value : ""} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+
+  removeLink(position) {
+    //удаляет звено цепи в указанном положении;
+    if (
+      typeof position !== "number" ||
+      position <= 0 ||
+      position > this.chain.length ||
+      !Number.isInteger(position)
+    ) {
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    //переворачивает цепь;
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    //заканчивает цепочку и returnsэто.
+    const result = this.chain.join("~~");
+    this.chain = [];
+    return result;
   },
 };
 
